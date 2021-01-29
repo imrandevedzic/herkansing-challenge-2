7d8542c9806ed583b3e71dabd3a25e61ab0d178b
imrandevedzic <63668335+imrandevedzic@users.noreply.github.com>
2021-01-29 13:31:23 +0100

    Add files via upload

---
+++ pg.js
@@ -0,0 +1,7 @@
+const hamburger=document.getElementById('hamburger');
+const menuUl=document.getElementById('menu-ul');
+hamburger.addEventListener('click',()=>{
+	menuUl.classList.toggle('show');
+}
+
+	)
